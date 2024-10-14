0c0446cce610674bc8bbfa82c5c83dd75893ef47
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-14 15:38:12 +0200

    add price calculation and discounts

--- js/script.js
+++ js/script.js
@@ -9,4 +9,13 @@ console.log(typeof userPassengerAge, userPassengerAge);
 
 // elaborazione
 
+const ticketPrice = userKmNumber * 0.21;
+console.log(typeof ticketPrice, ticketPrice);
+
+if (userPassengerAge <= 18) {
+  console.log("applica sconto 20%");
+} else if (userPassengerAge >= 65) {
+  console.log("applica sconto 40%");
+}
+
 // output
