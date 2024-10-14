1d26c9217c8cc4b587f4661e4d41f05a83443368
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-14 15:24:25 +0200

    add first prompts

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("JS OK");
+// raccolta dati
+const userKmNumber = parseInt(
+  prompt("Inserisci il numero di km che desideri percorrere")
+);
+console.log(typeof userKmNumber, userKmNumber);
+
+const userPassengerAge = parseInt(prompt("Inserisci l'età del passeggero"));
+console.log(typeof userPassengerAge, userPassengerAge);
+
+// elaborazione
+
+// output
